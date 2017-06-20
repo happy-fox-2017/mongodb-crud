db4fba1ed091f6d8ec98e933f548bd4f6a8ff413
adeanugerah89 <ade_anugerahtb@yahoo.com>
2017-06-20 14:50:41 +0700

    mongoDB done

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const ObjectId = require('mongodb').ObjectId;
+var ObjectID = require('mongodb').ObjectID; 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/library";
 
@@ -42,7 +42,52 @@ let createBook = (req,res)=>{
   })
 }
 
+let updateBook = (req,res)=>{
+  MongoClient.connect(url,(err,db)=>{
+    if (err) {
+      res.send(err)
+    }
+    // console.log(data);
+    db.collection('books').update({ 
+      _id : ObjectID(req.params.id)
+    },{
+      isbn : req.body.isbn,
+      title: req.body.title,
+      author: req.body.author ,
+      category: req.body.category ,
+      stock: req.body.stock 
+    },(err,result)=>{
+      if (err) {
+        res.send(err)
+      }
+      console.log(result);
+      res.send(result)
+      db.close()
+    })
+  })
+}
+
+let deleteBook =(req,res)=>{
+  MongoClient.connect(url,(err,db)=>{
+    if (err) {
+      res.send(err)
+    }
+    var id = {_id : ObjectID(req.params.id)}
+    db.collection('books').remove( id,(req,result)=>{
+      if (err) {
+        res.send(err)
+      }
+      console.log(result);
+      res.send(result)
+      db.close()
+    })
+  })
+}
+
+
 module.exports = {
   getAllBooks,
-  createBook
+  createBook,
+  updateBook,
+  deleteBook
 }; 
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,7 @@ var controller = require('../controllers/books_controller');
 /* GET users listing. */
 router.get('/', controller.getAllBooks);
 router.post('/', controller.createBook);
+router.put('/:id', controller.updateBook);
+router.delete('/:id', controller.deleteBook);
 
 module.exports = router;
