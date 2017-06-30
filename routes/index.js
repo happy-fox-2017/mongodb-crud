9675ec6b1393f5275ca1927e2394c1f5086bc27c
jumadiakhmad <jumadiakhmad2@gmail.com>
2017-07-01 00:56:38 +0700

    Mongodb CRUD Done

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.send('Welcome to the Example MongoDb..');
+router.get('/', function(req, res, next) {
+  res.render('Welcome to Mongodb Crud');
 });
 
 module.exports = router;
